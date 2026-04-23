4fa91c19df82353dc9eae6af5294623fb48eaa1a
Miguel Lozano <miguelozanodev@gmail.com>
2026-04-23 18:01:31 -0500

    crear LoginViewModel con estado observable Implementa 4 estados con mutableStateOf + private set: email, password, rememberMe y passwordVisible. Expone funciones onXxxChange y togglePasswordVisibility. Hereda de androidx.lifecycle.ViewModel para sobrevivir cambios de configuración.

--- app/src/main/java/com/julian/miju2/LoginModule/LoginViewModel.kt
+++ app/src/main/java/com/julian/miju2/LoginModule/LoginViewModel.kt
@@ -1,4 +1,40 @@
 package com.julian.miju2.LoginModule
 
-class LoginViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel: ViewModel() {
+
+    var email: String by mutableStateOf("")
+        private set
+    var password: String by mutableStateOf("")
+        private set
+    var rememberMe: Boolean by mutableStateOf(false)
+        private set
+
+    var passwordVisible: Boolean by mutableStateOf(false)
+        private set
+
+
+
+
+
+    fun onEmailChange(nuevoEmail: String){
+        email = nuevoEmail
+    }
+
+
+    fun onPasswordChange(nuevoPassword: String){
+        password = nuevoPassword
+    }
+
+    fun onRememberChange(nuevoValor: Boolean){
+        rememberMe = nuevoValor
+    }
+
+    fun togglePasswordVisibility(){
+        passwordVisible = !passwordVisible
+    }
 }
