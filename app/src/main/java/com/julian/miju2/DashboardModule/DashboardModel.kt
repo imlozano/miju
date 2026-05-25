a3ac247e3dd115da8b7273fcb83d0d1b0d3bd07c
Miguel Lozano <miguelozanodev@gmail.com>
2026-05-25 16:05:33 -0500

    feat(dashboard): modelo de datos Transaction

--- app/src/main/java/com/julian/miju2/DashboardModule/DashboardModel.kt
+++ app/src/main/java/com/julian/miju2/DashboardModule/DashboardModel.kt
@@ -1,4 +1,10 @@
 package com.julian.miju2.DashboardModule
 
-class DashboardModel {
-}
+data class Transaction(
+    val transactionId: String = "",
+    val from: String = "",
+    val to: String = "",
+    val amount: Double = 0.0,
+    val date: Long = 0L,
+    val status: String = ""
+)
